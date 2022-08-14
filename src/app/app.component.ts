748f1d941870b457a13c23f894fe0131bf5061a4
Abhay Ahire <abhayahire3@gmail.com>
2022-08-14 18:31:29 +0530

    VIEW BASE64 AND PRINT PDF 1. Added base64 image view 2. Print PDF using javascript for target view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'print-pdf';
+  base64String: any;
+  constructor(private dataService : DataService, private sanitizer: DomSanitizer) {
+
+  }
+
+  ngOnInit() {
+    this.dataService.getData().subscribe((baseImage:any) => {
+      let base64Source = 'data:image/jpeg;base64,' + baseImage.data;
+      this.base64String = this.sanitizer.bypassSecurityTrustUrl(base64Source);
+    })
+  }
+
+  printPDF() {
+    const targetElement : any = document.getElementById("printContainer");
+    const WindowPrt : any = window.open('', '', 'left=500,top=100,width=900,height=900,toolbar=0,scrollbars=0,status=0');
+    WindowPrt.document.write(targetElement.innerHTML);
+    WindowPrt.document.close();
+    WindowPrt.focus();
+    WindowPrt.print();
+    WindowPrt.close();
+  }
+
 }
